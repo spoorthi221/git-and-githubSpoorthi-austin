00df805c461c5eddb2add20d9323ae18c5fde12e
Austin Kao <ckeveyna@gmail.com>
2023-09-18 22:37:57 -0500

    finished

--- bottles.py
+++ bottles.py
@@ -1,16 +1,12 @@
 beverage = "fenta"
-count = 2
 
-print(count, "bottles of", beverage, "on the wall")
-print(count, "bottles of", "water")
-print("Take one down, pass it around")
-count = count - 1
-print(count, "bottles of", beverage, "on the wall")
+for count in range(100, 1, -1):
+    if(count == 5):
+        print("If one of those bottles should happen to fall, 98 bottles of beer on the wall...")
+    else:
+        print(count, "bottles of", beverage, "on the wall")
+        print(count, "bottles of", "fenta")
+        print("Take one down, pass it around")
 
-print("")
-
-print(count, "bottles of", beverage, "on the wall")
-print(count, "bottles of", beverage)
-print("Take one down, pass it around")
-count += 5
-print(count, "bottles of", beverage, "on the wall")
+print("No more bottles of beer on the wall, no more bottles of beer.")
+print("We've taken them down and passed them around; now we're drunk and passed out!")
